b7c9e9f37647d411413dffdfae1c9e6876f795d4
erick <efmoreira@inf.ufpel.edu.br>
2019-01-14 17:12:28 -0200

    Logando. Salvando e recuperando cartoes no localStorage

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,1 +1,16 @@
-let logado = true;
+let logado = JSON.parse(localStorage.getItem("logado"));
+
+LoginUsuario_render({
+  logado: logado,
+  usuario: localStorage.getItem('usuario'),
+  onLogin: (nome_usuario) => {
+    logado = true;
+    localStorage.setItem("logado", true);
+    localStorage.setItem("usuario", nome_usuario);
+  },
+  onLogout: () => {
+    logado = false;
+    localStorage.setItem("logado", false);
+    localStorage.removeItem("usuario");
+  }
+})
--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,25 +1,41 @@
 const Mural = (function(_render, Filtro) {
   "use strict"
-  let cartoes = []
+  let cartoes = leCartoes();
   const render = () => _render({
     cartoes: cartoes,
     filtro: Filtro.tagsETexto
   });
+  render()
 
   Filtro.on("filtrado", render)
 
   function adiciona(cartao) {
-    if (logado) {
+    if(logado){
       cartoes.push(cartao)
+      salvaCartoes()
       cartao.on("mudanca.**", render)
-      cartao.on("remocao", () => {
-        cartoes = cartoes.slice(0)
-        cartoes.splice(cartoes.indexOf(cartao), 1)
-        render()
+      cartao.on("remocao", ()=>{
+          cartoes = cartoes.slice(0)
+          cartoes.splice(cartoes.indexOf(cartao),1)
+          render()
       })
+      render()
+      return true
     } else {
-      alert('Você não está logado');
+      alert("Você não está logado")
     }
+  }
+
+  function leCartoes(){
+    return JSON.parse(localStorage.getItem('cartoes')).map(
+      (cartaoLocal) => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo)
+      ) || []
+  }
+
+  function salvaCartoes(){
+    let cartoes_mapped = cartoes.map((cartao) => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+
+    localStorage.setItem('cartoes', JSON.stringify(cartoes_mapped));
   }
 
   return Object.seal({
